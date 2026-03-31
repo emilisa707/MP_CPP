70cdf825a7862a7e4da64590cf19e90a9bbb425f
Emilisa <emilisa90@gmail.com>
2026-03-31 21:57:02 +0700

    4. LAN Connection

---
+++ Source/MP_CPP/LAN/MP_LANMenu.cpp
@@ -0,0 +1,36 @@
+﻿#include "MP_LANMenu.h"
+
+#include "Components/Button.h"
+#include "Components/EditableTextBox.h"
+#include "Kismet/GameplayStatics.h"
+
+void UMP_LANMenu::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+	
+	FInputModeUIOnly InputMode;
+	GetOwningPlayer()->SetInputMode(InputMode);
+	GetOwningPlayer()->SetShowMouseCursor(true);
+	
+	Button_Host->OnClicked.AddDynamic(this, &ThisClass::HostButtonClicked);
+	Button_Join->OnClicked.AddDynamic(this, &ThisClass::JoinButtonClicked);
+}
+
+void UMP_LANMenu::JoinButtonClicked()
+{
+	FInputModeGameOnly InputMode;
+	GetOwningPlayer()->SetInputMode(InputMode);
+	GetOwningPlayer()->SetShowMouseCursor(false);
+	
+	const FString Address = TextBox_IpAddress->GetText().ToString();
+	UGameplayStatics::OpenLevel(this, *Address);
+}
+
+void UMP_LANMenu::HostButtonClicked()
+{
+	FInputModeGameOnly InputMode;
+	GetOwningPlayer()->SetInputMode(InputMode);
+	GetOwningPlayer()->SetShowMouseCursor(false);
+	
+	UGameplayStatics::OpenLevelBySoftObjectPtr(this, HostingLevel, true, TEXT("listen"));
+}
