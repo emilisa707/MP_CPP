43a1a78b809baba6eb49781c9dcbce7b9be36cb8
Emilisa <emilisa90@gmail.com>
2026-04-02 16:52:50 +0700

    MP_Actor

---
+++ Source/MP_CPP/Actors/MP_Actor.cpp
@@ -0,0 +1,25 @@
+﻿#include "MP_Actor.h"
+
+
+AMP_Actor::AMP_Actor()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	
+	bNetLoadOnClient = true;
+	bReplicates = true;
+	SetReplicatingMovement(true);
+}
+
+// Called when the game starts or when spawned
+void AMP_Actor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMP_Actor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
