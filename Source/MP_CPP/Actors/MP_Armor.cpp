4d84484905d0188738434228769b80e34902bc54
Emilisa <emilisa90@gmail.com>
2026-04-29 11:13:06 +0700

    8. Attachment

---
+++ Source/MP_CPP/Actors/MP_Armor.cpp
@@ -0,0 +1,48 @@
+﻿#include "MP_Armor.h"
+
+#include "Components/SphereComponent.h"
+#include "GameFramework/Character.h"
+
+
+// Sets default values
+AMP_Armor::AMP_Armor()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	SetRootComponent(CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent")));
+	
+	SphereMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("SphereMesh"));
+	SphereMesh->SetupAttachment(RootComponent);
+	SphereMesh->SetIsReplicated(true);
+	
+	SphereFootL = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("SphereFootL"));
+	SphereFootL->SetupAttachment(RootComponent);
+	SphereFootL->SetIsReplicated(true);
+	
+	SphereFootR = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("SphereFootR"));
+	SphereFootR->SetupAttachment(RootComponent);
+	SphereFootR->SetIsReplicated(true);
+	
+	SphereCollision = CreateDefaultSubobject<USphereComponent>(TEXT("SphereCollision"));
+	SphereCollision->SetupAttachment(RootComponent);
+}
+
+void AMP_Armor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AMP_Armor::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+	
+	ACharacter* OverlappedCharacter = Cast<ACharacter>(OtherActor);
+	if (HasAuthority() && IsValid(OverlappedCharacter))
+	{
+		//AttachToActor(OtherActor, FAttachmentTransformRules::SnapToTargetNotIncludingScale);
+		SphereMesh->AttachToComponent(OverlappedCharacter->GetMesh(), FAttachmentTransformRules::SnapToTargetNotIncludingScale, FName("head"));
+		SphereFootL->AttachToComponent(OverlappedCharacter->GetMesh(), FAttachmentTransformRules::SnapToTargetNotIncludingScale, FName("foot_L"));
+		SphereFootR->AttachToComponent(OverlappedCharacter->GetMesh(), FAttachmentTransformRules::SnapToTargetNotIncludingScale, FName("foot_R"));
+	}
+}
+
---
+++ Source/MP_CPP/Actors/MP_Armor.h
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MP_Armor.generated.h"
+
+class USphereComponent;
+
+UCLASS()
+class MP_CPP_API AMP_Armor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AMP_Armor();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void NotifyActorBeginOverlap(AActor* OtherActor) override;
+
+private:
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	TObjectPtr<UStaticMeshComponent> SphereMesh;
+	
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	TObjectPtr<UStaticMeshComponent> SphereFootL;
+	
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	TObjectPtr<UStaticMeshComponent> SphereFootR;
+	
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	TObjectPtr<USphereComponent> SphereCollision;
+};
